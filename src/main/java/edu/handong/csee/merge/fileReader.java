e5f6eaa9cb4fbc50a3e21d647ce30b6ebaa08b1e
1nhee <21800025@handong.edu>
2019-06-06 21:24:35 +0900

    fi

--- src/main/java/edu/handong/csee/merge/fileReader.java
+++ src/main/java/edu/handong/csee/merge/fileReader.java
@@ -20,108 +20,68 @@ import java.util.LinkedList;
 
 public class fileReader { 
 	
-	public Class fileReader{
-		
-	}
-	
-	
-	ArrayList<String> strTemp;
-    String lock = "test" ;
-    String strOutput;
-    String strInput;
-    Queue<String> ql = new LinkedList<String>();
-    
-    class TextFileReader extends Thread
-    {
-        public void run()
-        {
-            //System.out.println(new Date());
-            
-            try
-            {
-                 
-                BufferedReader br = new BufferedReader
-                      (new FileReader("test.txt"));
-                 
-                while(true)
-                {
-                    strTemp.add(br.readLine());
-                    if (strTemp == null)
-                    {
-                        br.close();
-                        ql.offer("eof");
-                        break;
-                    }
-                    synchronized(lock)
-                    {
-                        ql.offer(strTemp);
-                        lock.notifyAll();
-                        // this is to prevent out of memory error
-                        if (ql.size() > 1000)
-                        {
-                            try
-                            {
-                                lock.wait();
-                            }
-                            catch(InterruptedException ex)
-                            {
-                            }
-                        }
-                    }
-                }
-                 
-            }//outer while end
-            catch(IOException ex)
-            {
-            }
-            catch(OutOfMemoryError eo)
-            {
-                System.out.println("queue size " + ql.size());
-            }//outer try end
-        }//run method end
-    }//TextReader class end
+	public void readAFile() {
+	try { 
+        FileInputStream file = new FileInputStream(new File("gfgcontribute.xlsx")); 
 
+        // Create Workbook instance holding reference to .xlsx file 
+        XSSFWorkbook workbook = new XSSFWorkbook(file); 
+
+        // Get first/desired sheet from the workbook 
+        XSSFSheet sheet = workbook.getSheetAt(0); 
+
+        // Iterate through each rows one by one 
+        Iterator<Row> rowIterator = sheet.iterator(); 
+        while (rowIterator.hasNext()) { 
+            Row row = rowIterator.next(); 
+            // For each row, iterate through all the columns 
+            Iterator<Cell> cellIterator = row.cellIterator(); 
+
+            while (cellIterator.hasNext()) { 
+                Cell cell = cellIterator.next(); 
+                // Check the cell type and format accordingly 
+                switch (cell.getCellType()) { 
+                case Cell.CELL_TYPE_NUMERIC: 
+                    System.out.print(cell.getNumericCellValue() + "t"); 
+                    break; 
+                case aCell.CELL_TYPE_STRING: 
+                    System.out.print(cell.getStringCellValue() + "t"); 
+                    break; 
+                } 
+            } 
+            System.out.println(""); 
+        } 
+        file.close(); 
+    } 
+    catch (Exception e) { 
+        e.printStackTrace(); 
+    } 
+	
+	}
+} 
+	
+	
+	
+	
 	/*
-	 * class MultiThread implements Runnable{ private static BufferedReader br =
-	 * null; private List<String> list = new List<String>;
-	 * 
-	 * try { br = new BufferedReader(new FileReader("F://ThreadDemo.txt"),10);
-	 * }catch (FileNotFoundException e) { // TODO Auto-generated catch block
-	 * e.printStackTrace(); }
-	 * 
-	 * public void run() { String line = null; int count = 0;
-	 * 
-	 * while(true) { //System.out.println(Thread.currentThread().getName());
-	 * this.list = new ArrayList<String>(); synchronized(br) { try {
-	 * 
-	 * FileInputStream excelFile = new FileInputStream(new File(FILE_NAME));
-	 * Workbook workbook = new XSSFWorkbook(excelFile); Sheet datatypeSheet =
-	 * workbook.getSheetAt(0); Iterator<Row> iterator = datatypeSheet.iterator();
-	 * 
-	 * while (iterator.hasNext()) {
-	 * 
-	 * Row currentRow = iterator.next(); Iterator<Cell> cellIterator =
-	 * currentRow.iterator();
-	 * 
-	 * while (cellIterator.hasNext()) {
-	 * 
-	 * Cell currentCell = cellIterator.next(); //getCellTypeEnum shown as deprecated
-	 * for version 3.15 //getCellTypeEnum ill be renamed to getCellType starting
-	 * from version 4.0 if (currentCell.getCellTypeEnum() == CellType.STRING) {
-	 * System.out.print(currentCell.getStringCellValue() + "--"); } else if
-	 * (currentCell.getCellTypeEnum() == CellType.NUMERIC) {
-	 * System.out.print(currentCell.getNumericCellValue() + "--"); }
-	 * 
-	 * } System.out.println();
-	 * 
-	 * } } catch (FileNotFoundException e) { e.printStackTrace(); } catch
-	 * (IOException e) { e.printStackTrace(); }
-	 * 
-	 * } } try { Thread.sleep(1); } catch (InterruptedException e) {
-	 * e.printStackTrace(); } if(line == null) break; }
+	 * ArrayList<String> strList; String strTemp; String lock = "test" ;
+	 * Queue<String> ql = new LinkedList<String>();
 	 * 
 	 * 
-	 * }
+	 * 
+	 * class TextFileReader extends Thread { public void run() { try {
+	 * 
+	 * BufferedReader br = new BufferedReader (new FileReader("test.txt"));
+	 * 
+	 * while(true) { strTemp = br.readLine(); strList.add(strTemp); if (strTemp ==
+	 * null) { br.close(); ql.offer("eof"); break; } synchronized(lock) {
+	 * ql.offer(strTemp); lock.notifyAll(); // this is to prevent out of memory
+	 * error if (ql.size() > 1000) { try { lock.wait(); } catch(InterruptedException
+	 * ex) { } } } }
+	 * 
+	 * }//outer while end catch(IOException ex) { } catch(OutOfMemoryError eo) {
+	 * System.out.println("queue size " + ql.size()); }//outer try end }//run method
+	 * end }//TextReader class end
 	 */
 	
 }
