d853f60618ec7cd39e12c7b7e9c37e5f015a8450
1nhee <21800025@handong.edu>
2019-06-06 20:50:37 +0900

    edi

--- src/main/java/edu/handong/csee/merge/UnzipFiles.java
+++ src/main/java/edu/handong/csee/merge/UnzipFiles.java
@@ -10,47 +10,70 @@ import java.util.zip.ZipInputStream;
 
 public class UnzipFiles {
 
-    private static ArrayList<String> unzip(String zipFilePath, String destDir) {
-    	
-        File dir = new File(destDir);
-        // create output directory if it doesn't exist
-        if(!dir.exists()) dir.mkdirs();
-        FileInputStream fis;
-        //buffer for read and write data to file
-        byte[] buffer = new byte[1024];
-        
-        try {
-            fis = new FileInputStream(zipFilePath);
-            ZipInputStream zis = new ZipInputStream(fis);
-            ZipEntry ze = zis.getNextEntry();
-            ArrayList<String> zipFileName = new ArrayList<String>();
-            
-            while(ze != null){
-                String fileName = ze.getName();
-                zipFileName.add(fileName);
-                
-                File newFile = new File(destDir + File.separator + fileName);
-                System.out.println("Unzipping to "+newFile.getAbsolutePath());
-                
-				/*
-				 * //create directories for sub directories in zip new
-				 * File(newFile.getParent()).mkdirs(); FileOutputStream fos = new
-				 * FileOutputStream(newFile); int len; while ((len = zis.read(buffer)) > 0) {
-				 * fos.write(buffer, 0, len); } fos.close();
-				 */
-                
-                //close this ZipEntry
-                zis.closeEntry();
-                ze = zis.getNextEntry();
-            }
-            //close last ZipEntry
-            zis.closeEntry();
-            zis.close();
-            fis.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-       return zipFileName;
-    }
+	public static void decompress(String zipFileName, String directory) throws Throwable {
+		File zipFile = new File(zipFileName);
+		FileInputStream fis = null;
+		ZipInputStream zis = null;
+		ZipEntry zipentry = null;
+		try {
+		//파일 스트림
+		fis = new FileInputStream(zipFile);
+		//Zip 파일 스트림
+		zis = new ZipInputStream(fis);
+		//entry가 없을때까지 뽑기
+		while ((zipentry = zis.getNextEntry()) != null) {
+		String filename = zipentry.getName();
+		File file = new File(directory, filename);
+		//entiry가 폴더면 폴더 생성
+		if (zipentry.isDirectory()) {
+		file.mkdirs();
+		} else {
+		//파일이면 파일 만들기
+		createFile(file, zis);
+		}
+		}
+		} catch (Throwable e) {
+		throw e;
+		} finally {
+		if (zis != null)
+		zis.close();
+		if (fis != null)
+		fis.close();
+		}
+		}
+		/**
+		* 파일 만들기 메소드
+		* @param file 파일
+		* @param zis Zip스트림
+		*/
+		private static void createFile(File file, ZipInputStream zis) throws Throwable {
+		//디렉토리 확인
+		File parentDir = new File(file.getParent());
+		//디렉토리가 없으면 생성하자
+		if (!parentDir.exists()) {
+		parentDir.mkdirs();
+		}
+		//파일 스트림 선언
+		try (FileOutputStream fos = new FileOutputStream(file)) {
+		byte[] buffer = new byte[256];
+		int size = 0;
+		//Zip스트림으로부터 byte뽑아내기
+		while ((size = zis.read(buffer)) > 0) {
+		//byte로 파일 만들기
+		fos.write(buffer, 0, size);
+		}
+		} catch (Throwable e) {
+		throw e;
+		}
+		}
+		public static void main(String[] args){
+		try{
+		Zip.decompress("D:\\test.zip", "D:\\test1");
+		}catch(Throwable e){
+		e.printStackTrace();
+		}
+		}
+		}
+
 
 }
