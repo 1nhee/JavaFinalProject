e2c12f94c52ed46556de66548c5b15a0381db8e1
1nhee <21800025@handong.edu>
2019-06-10 09:08:37 +0900

    a

--- src/main/java/edu/handong/csee/merge/CSVWriter.java
+++ src/main/java/edu/handong/csee/merge/CSVWriter.java
@@ -11,20 +11,39 @@ import java.util.Arrays;
 
 public class CSVWriter {
 
-    public void writeAFileArrayList (ArrayList<Object> finishStr, String targetFileName, ArrayList<Object> allHeader) throws IOException {
-        try (
+	public static void writeAFile (ArrayList<Object> finishStr, String targetFileName, ArrayList<Object> allHeader) throws IOException {
+        try {
+
             BufferedWriter writer = Files.newBufferedWriter(Paths.get(targetFileName));
+        		
+        	String head = new String();
+        	for(Object header : allHeader) {
+        		System.out.println("object "+header);
+        		head.concat(header.toString());
+        	}
+        	System.out.println("head "+head);
 
             CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT
-                    .withHeader("ID", "Name", "Designation", "Company"));
-        ) {
-            csvPrinter.printRecord("1", "Sundar Pichai ¢¾", "CEO", "Google");
-            csvPrinter.printRecord("2", "Satya Nadella", "CEO", "Microsoft");
-            csvPrinter.printRecord("3", "Tim cook", "CEO", "Apple");
-
-            csvPrinter.printRecord(Arrays.asList("4", "Mark Zuckerberg", "CEO", "Facebook"));
-
+                    .withHeader("ID", "Name", "Designation", "Company")); 
+            
+            int count = 0;
+            String input = new String();
+            for(Object in : finishStr) {
+            	if(count == 6) {
+            		input.concat(in.toString());
+            		System.out.println("input "+input);
+            		csvPrinter.printRecord(input);
+            		count = 0;
+            		input = "";
+            	}else {
+            		input.concat(in.toString()+",");
+                	count++;
+            	}
+            }
+            System.out.println("File is written in " + targetFileName);
             csvPrinter.flush();            
+        }finally{
+        	
         }
-    }
+}
 }
