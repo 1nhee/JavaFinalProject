6298b0bf3fa6e751ae98a6d530c380c4efc92a4f
1nhee <21800025@handong.edu>
2019-06-11 18:03:18 +0900

    a

--- src/main/java/edu/handong/csee/merge/ExcelReader.java
+++ src/main/java/edu/handong/csee/merge/ExcelReader.java
@@ -1,53 +1,22 @@
 package edu.handong.csee.merge;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-
 import org.apache.poi.ss.usermodel.CellType;
-import org.apache.poi.ss.usermodel.DateUtil;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
-
 import edu.handong.csee.merge.MyException;
 
 public class ExcelReader {
 
-	public ArrayList<String> getData(String path) {
-		ArrayList<String> values = new ArrayList<String>();
-
-		System.out.println(path);
-
-		try (InputStream inp = new FileInputStream(path)) {
-			// InputStream inp = new FileInputStream("workbook.xlsx");
-
-			Workbook wb = WorkbookFactory.create(inp);
-			Sheet sheet = wb.getSheetAt(0);
-			Row row = sheet.getRow(2);
-			Cell cell = row.getCell(1);
-			if (cell == null)
-				cell = row.createCell(3);
-
-			values.add(cell.getStringCellValue());
-
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		return values;
-	}
-
 	public ArrayList<String> getData(InputStream is) {
 		ArrayList<String> values = new ArrayList<String>();
+		ArrayList<String> error = new ArrayList<String>();
 
 		try (InputStream inp = is) {
 			
@@ -55,6 +24,10 @@ public class ExcelReader {
 			Sheet sheet = wb.getSheetAt(0);
 
 			for (Row row : sheet) {
+				
+				if(row.getLastCellNum() != 5 || row.getLastCellNum() != 7 ) {
+					throw new MyException();
+				}
 				
 				if (row.getRowNum() == 0) {
 					String toAdd = Integer.toString(row.getLastCellNum());
@@ -83,6 +56,9 @@ public class ExcelReader {
 			// TODO Auto-generated catch block
 			//MyException ex = new MyException();
 			//e.printStackTrace();
+		} catch (MyException e) {
+			System.out.println(e.getMessage());
+			error.add("error");
 		}
 
 		return values;
